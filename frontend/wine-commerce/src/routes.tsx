a880f9abf27d8539610fa342e6aa01887f000527
João Pedro Basso Audibert <hotsjoao@gmail.com>
2022-05-09 22:49:27 -0300

    🎨 pages added to routing

--- frontend/wine-commerce/src/routes.tsx
+++ frontend/wine-commerce/src/routes.tsx
@@ -5,12 +5,16 @@ import {
 } from "react-router-dom";
 
 import Home from "./pages/Home";
+import ProductsList from "./pages/ProductsList";
+import Login from "./pages/Login";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductsList />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
